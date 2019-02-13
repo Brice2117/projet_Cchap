e4628c7ca3778e43fc5aec9683f1d6c833e8202f
MaxanceMallet666 <40290422+MaxanceMallet666@users.noreply.github.com>
2019-02-13 09:52:28 +0100

    Add files via upload

---
+++ Pause_Menu_Perso.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause_Menu_Perso : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused==true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuiTGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("Level_1");
+        Time.timeScale = 1f;
+    }
+}
---
+++ Perso_Info_Printing.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Perso_Info_Printing : MonoBehaviour
+{
+    public GameObject lifeText;
+    public GameObject failMenu;
+    public int nbOfLives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        failMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        lifeText.GetComponent<TextMeshProUGUI>().text = "x"+nbOfLives.ToString();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Vide" && nbOfLives>0)
+        {
+            nbOfLives--;
+            lifeText.GetComponent<TextMeshProUGUI>().text = "x"+nbOfLives.ToString();
+        }
+        else
+        {
+            failMenu.gameObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

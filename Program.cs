5ce78e5a174db0e11583da11b29568e6b62cc4ba
Elie Sidawi <45319735+eliesidawi@users.noreply.github.com>
2019-03-04 16:49:35 +0100

    CODE POUR SCORE

--- Program.cs
+++ Program.cs
@@ -1,63 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
 
-using _Excel= Microsoft.Office.Interop.Excel;
-
-namespace Score_Excel
+namespace TXT
 {
-    class Program
+    class WriteTextFile
     {
-            string path = "";
-        _Application excel = new _Excel.Application();
-        Workbook wb;
-        Worksheet ws;
-        public static void Main(string []args)
+        static void Main()
         {
-            //OpenFile();
-            Program excel = new Program(@"C:\Users\Utilisateur\Desktop\Score_Excel\Score_Excel\Tst.xlsx", 1);
-            Console.WriteLine(excel.ReadCell(0, 0));
-            Console.WriteLine("Enter Somting to set to a cell");
-            string txt=Console.ReadLine();
-            Console.WriteLine("Enter the row number");
-            int row= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the col number");
-            int col= Convert.ToInt32(Console.ReadLine());
-            excel.WriteToCell(row, col, txt);
-            excel.Save();
-        }
-        public Program(string path, int sheet)
-        {
-            this.path = path;
-            wb = excel.Workbooks.Open(path);
-            ws = wb.Worksheets[sheet];
 
-        }
+            string[] names = new string[1];
+            string[] times = new string[1];
 
-        public string ReadCell(int i, int j)
-        {
-            i++;
-            j++;
-            if (ws.Cells[i, j].Value2 != null)
+            string[] n = { " ", " ", " ", " ", " " };
+            string[] t = { "0,0", "0,0", "0,0", "0,0", "0,0" };
+
+            try//si les 2 fichiers n'existent pas
             {
-                return ws.Cells[i, j].Value2;
+                n = System.IO.File.ReadAllLines(@"Names.txt");
+                t = System.IO.File.ReadAllLines(@"Times.txt");
             }
-            else { return ""; }
-        }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                System.IO.File.WriteAllLines(@"Names.txt", names);
+                System.IO.File.WriteAllLines(@"Times.txt", times);
+            }
 
-        public void WriteToCell(int i,int j,string s)
-        {
-            i++;
-            j++;
-            ws.Cells[i, j].Value2 = s;
-        }
-        public void Save()
-        {
-            wb.Save();
-            wb.Close();
+            //Get name and time
+            
+            string nom = "HELL";    //TO BE ADDED FROM UNITY
+            string temps = "0,1";   //TO BE ADDED FROM UNITY
+
+            string[] nN = { " ", " ", " ", " ", " ", " " };
+            string[] nT = { "0,0", "0,0", "0,0", "0,0", "0,0" ,"0,0"};
+
+            string[] nnT = System.IO.File.ReadAllLines(@"Times.txt");
+            int len = nnT.Length;
+            for (int i = 0; i < len; i++)
+            {
+                nT[i] = System.IO.File.ReadAllLines(@"Times.txt")[i];
+                nN[i] = System.IO.File.ReadAllLines(@"Names.txt")[i];
+            }
+
+            int indice = 0;
+            for(int i=0;i<5;i++)
+            {
+                if(nN[i] == " ")
+                {
+                    break;
+                }
+                indice = i;
+            }
+
+            if (nN[5]!=" ")
+            {
+                nN = new string[6] { " ", " ", " ", " ", " "," " };
+                nT = new string[6] { "0,0", "0,0", "0,0", "0,0", "0,0", "0,0" };
+            }
+            else
+            {
+                nN[indice + 1] = nom;
+                nT[indice + 1] = temps;
+            }
+
+            if (n[0]!=" ")
+            {
+                string temp1;
+                string temp2;
+
+                for (int j = 0; j <= 4; j++)//Bubble sort to sort the two arrays
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (Convert.ToDouble(nT[i]) > Convert.ToDouble(nT[i + 1]))//si le temps i+1 est plus grand que le temps i
+                        {
+                            temp1 = nT[i + 1];
+                            nT[i + 1] = nT[i];
+                            nT[i] = temp1;
+
+                            temp2 = nN[i + 1];
+                            nN[i + 1] = nN[i];
+                            nN[i] = temp2;
+                        }
+                    }
+                }
+                System.IO.File.WriteAllLines(@"Names.txt", nN);//set new values for the names
+                System.IO.File.WriteAllLines(@"Times.txt", nT);//set new values for the times
+            }
+            else
+            {
+                names[0] = nom;
+                times[0] = temps;
+                System.IO.File.WriteAllLines(@"Names.txt", names);
+                System.IO.File.WriteAllLines(@"Times.txt", times);
+            }
+
         }
     }
 }
+

6b028e705c439706e60d99dda1d595eaa40498df
Elie Sidawi <45319735+eliesidawi@users.noreply.github.com>
2019-03-04 18:48:29 +0100

    DataBase TEXT File
    Ce qui marche bien

--- Program.cs
+++ Program.cs
@@ -6,68 +6,61 @@ namespace TXT
     {
         static void Main()
         {
+            //Get name and time
+            string nom = "ElieSSS";    //TO BE ADDED FROM UNITY
+            string temps = "7,1";    //TO BE ADDED FROM UNITY
 
-            string[] names = new string[1];
-            string[] times = new string[1];
-
-            string[] n = { " ", " ", " ", " ", " " };
-            string[] t = { "0,0", "0,0", "0,0", "0,0", "0,0" };
-
-            try//si les 2 fichiers n'existent pas
+            string[] nN = { " ", " ", " ", " ", " " };//String array for names
+            string[] nT = { "9999999,0", "9999999,0", "9999999,0", "9999999,0", "9999999,0" };//String array for times
+            int len = 0;
+            try
             {
-                n = System.IO.File.ReadAllLines(@"Names.txt");
-                t = System.IO.File.ReadAllLines(@"Times.txt");
+                string[] nnT = System.IO.File.ReadAllLines(@"Times.txt");//used to get the length of the saved data
+                len = nnT.Length;
+                Console.WriteLine("length: " + len);
             }
             catch(Exception e)
             {
                 Console.WriteLine(e);
-                System.IO.File.WriteAllLines(@"Names.txt", names);
-                System.IO.File.WriteAllLines(@"Times.txt", times);
             }
 
-            //Get name and time
-            
-            string nom = "HELL";    //TO BE ADDED FROM UNITY
-            string temps = "0,1";   //TO BE ADDED FROM UNITY
-
-            string[] nN = { " ", " ", " ", " ", " ", " " };
-            string[] nT = { "0,0", "0,0", "0,0", "0,0", "0,0" ,"0,0"};
-
-            string[] nnT = System.IO.File.ReadAllLines(@"Times.txt");
-            int len = nnT.Length;
             for (int i = 0; i < len; i++)
             {
                 nT[i] = System.IO.File.ReadAllLines(@"Times.txt")[i];
                 nN[i] = System.IO.File.ReadAllLines(@"Names.txt")[i];
             }
 
-            int indice = 0;
-            for(int i=0;i<5;i++)
+            int indice = 0;//Pour savoir notre indice
+            for(int i=0;i<5;i++)// pour calculer l'indice
             {
-                if(nN[i] == " ")
+                if (nN[i] == " ")
                 {
+                    indice = i;
                     break;
                 }
-                indice = i;
             }
 
-            if (nN[5]!=" ")
+            if (nN[4]!=" ")//si elle est complete on la reset
             {
-                nN = new string[6] { " ", " ", " ", " ", " "," " };
-                nT = new string[6] { "0,0", "0,0", "0,0", "0,0", "0,0", "0,0" };
+                nN = new string[5] { " ", " ", " ", " ", " " };
+                nT = new string[5] { "9999999,0", "9999999,0", "9999999,0", "9999999,0", "9999999,0" };
             }
-            else
+            else// si elle n'est pas complete on ajout les nouveau valeurs
             {
-                nN[indice + 1] = nom;
-                nT[indice + 1] = temps;
+                if (indice != 0)
+                {
+                    nN[indice] = nom;
+                    nT[indice] = temps;
+                    Console.WriteLine("indice: " + indice);
+                }
             }
 
-            if (n[0]!=" ")
+            if (nN[0]!=" ")//si il y a deja des valeurs
             {
                 string temp1;
                 string temp2;
-
-                for (int j = 0; j <= 4; j++)//Bubble sort to sort the two arrays
+                //Bubble sort to sort the two arrays
+                for (int j = 0; j < 4; j++)
                 {
                     for (int i = 0; i < 4; i++)
                     {
@@ -85,15 +78,16 @@ namespace TXT
                 }
                 System.IO.File.WriteAllLines(@"Names.txt", nN);//set new values for the names
                 System.IO.File.WriteAllLines(@"Times.txt", nT);//set new values for the times
+                Console.WriteLine("entered in if cond");
             }
-            else
+            else if(nN[0] == " ")// si la liste est initialement vide
             {
-                names[0] = nom;
-                times[0] = temps;
-                System.IO.File.WriteAllLines(@"Names.txt", names);
-                System.IO.File.WriteAllLines(@"Times.txt", times);
+                nN[0] = nom;
+                nT[0] = temps;
+                System.IO.File.WriteAllLines(@"Names.txt", nN);
+                System.IO.File.WriteAllLines(@"Times.txt", nT);
+                Console.WriteLine("entered in else cond");
             }
-
         }
     }
 }
